e1cacdd7918746f4fc4f057a3e782d3d9555fe05
Bhutanisachin19 <sachin.bhutani60@gmail.com>
2021-11-05 11:42:12 +0530

    EDIT

--- src/test.js
+++ src/test.js
@@ -7,6 +7,9 @@ const Test = () => {
   const [chat, setChat] = useState([]);
   const [msg, setMsg] = useState("");
 
+  const [eMsgId, setEMsgId] = useState();
+  const [edit, setEdit] = useState(false);
+
   const db = firebase.firestore();
 
   // useEffect(() => {
@@ -22,7 +25,7 @@ const Test = () => {
 
   useEffect(() => {
     // setChat([])
-    console.log("useEffcwt ca;es");
+    console.log("useEffect Called");
 
     db.collection("chatting")
       .orderBy("createdAt")
@@ -60,19 +63,33 @@ const Test = () => {
   const sendMsg2 = async () => {
     // setChat([]);
     document.getElementById("tex").value = "";
-    const done = await db.collection("chatting").add({
-      name: msg,
-      createdAt: Date.now(),
-      type: "sachin",
-    });
-    console.log(done);
 
-    setMsg("");
+    console.log("Msg", msg);
+
+    if (edit) {
+      db.collection("chatting").doc(eMsgId).update({ name: msg }); //for edit
+      setEMsgId("");
+      setEdit(false);
+    } else {
+      const done = await db.collection("chatting").add({
+        name: msg,
+        createdAt: Date.now(),
+        type: "sachin",
+      });
+      console.log(done);
+      setMsg("");
+    }
   };
 
   const deleteChat = async (chatId) => {
     const res = await db.collection("chatting").doc(chatId).delete();
     // db.collection("chatting").doc(chatId).update({ name: "bar" }); //for edit
+  };
+
+  const editMsg = (msgToBeEdited) => {
+    // console.log("Hello", msgToBeEdited);
+    document.getElementById("tex").value = msgToBeEdited;
+    document.getElementById("tex").focus();
   };
 
   return (
@@ -168,6 +185,22 @@ const Test = () => {
                     fill-rule="evenodd"
                     d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
                   />
+                </svg>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="white"
+                  class="bi bi-pencil"
+                  viewBox="0 0 16 16"
+                  onClick={() => {
+                    setEMsgId(chat.id);
+                    setEdit(true);
+                    editMsg(chat.data.name);
+                  }}
+                >
+                  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                 </svg>
               </div>
             )}
